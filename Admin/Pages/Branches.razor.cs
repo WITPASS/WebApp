61ad61548801bf2ad34449a00fa51809bf9fe7a4
Mansab Khan <mansab@outlook.com>
2019-05-01 04:38:38 -0700

    add basic branch managenet #26

---
+++ Admin/Pages/Branches.razor.cs
@@ -0,0 +1,26 @@
+﻿using Data;
+using System;
+using System.Threading.Tasks;
+
+namespace Admin.Pages
+{
+    public class BranchesComponent : BaseComponent<Branch>
+    {
+        public BranchesComponent() : base("api/branches") { }
+
+        internal Guid BranchId { get; private set; }
+        protected override async Task OnInitAsync()
+        {
+            BranchId = Api.BranchId;
+
+            await base.OnInitAsync();
+        }
+
+        protected void Select(Branch branch)
+        {
+            Api.BranchId = BranchId = branch.Id;
+            Api.Branch = branch.Name;
+            Api.LoginInfoHasChanaged();
+        }
+    }
+}
