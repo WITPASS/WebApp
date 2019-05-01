4f70e80ceaddd28dfeb53a3414f253c78e02d08a
Mansab Khan <mansab@outlook.com>
2019-05-01 04:14:45 -0700

    add blazor configuration injectable service #21

--- Admin/Startup.cs
+++ Admin/Startup.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
+using System.Reflection;
 
 namespace Admin
 {
@@ -10,23 +15,38 @@ namespace Admin
     {
         public void ConfigureServices(IServiceCollection services)
         {
-           
+            services.AddSingleton<IConfiguration>(s =>
+            {
+                var settings = new Dictionary<string, string>();
+                var uri = new Uri(s.GetRequiredService<IUriHelper>().GetBaseUri());
+                var settingsFile = uri.IsLoopback ? "appsettings.Development.json" : "appsettings.json";
+
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(settingsFile))
+                using (var reader = new StreamReader(stream))
+                {
+                    var json = JObject.Parse(reader.ReadToEnd());
+
+                    foreach (var obj in json.Properties())
+                    {
+                        settings.Add(obj.Name, obj.Value.ToString());
+                    }
+                }
+
+                ConfigurationBuilder builder = new ConfigurationBuilder();
+                builder.AddInMemoryCollection(settings);
+
+                return builder.Build();
+            });
+
+            services.AddSingleton(typeof(ApiService));
+            services.AddSingleton(typeof(UtilsService));
         }
 
         public void Configure(IComponentsApplicationBuilder app)
         {
             var http = app.Services.GetRequiredService<HttpClient>();
-            var uriHelper = app.Services.GetRequiredService<IUriHelper>();
-            var uri = new Uri(uriHelper.GetBaseUri());
-
-            if (uri.IsLoopback)
-            {
-                http.BaseAddress = new Uri("http://localhost:50567/");
-            }
-            else
-            {
-                http.BaseAddress = new Uri("http://api.localhost/");
-            }
+            var config = app.Services.GetRequiredService<IConfiguration>();
+            http.BaseAddress = new Uri(config["API_ENDPOINT"]);
 
             app.AddComponent<App>("app");
         }
