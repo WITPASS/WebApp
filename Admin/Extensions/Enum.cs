8508fcb4b43c6db6b5fae3ba8547c5d9b1d5259a
Mansab Khan <mansab@outlook.com>
2019-05-20 11:35:01 -0700

    add form controls #42

---
+++ Admin/Extensions/Enum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin
+{
+    public class Enum<T> where T : struct, IConvertible
+    {
+        public static IList<T> TypedList
+        {
+            get
+            {
+                if (!typeof(T).IsEnum) throw new ArgumentException("T must be an enumerated type");
+
+                var list = new List<T>();
+
+                foreach (var en in Enum.GetValues(typeof(T))) list.Add((T)en);
+
+                return list;
+            }
+        }
+
+        public static int Count
+        {
+            get
+            {
+                if (!typeof(T).IsEnum) throw new ArgumentException("T must be an enumerated type");
+
+                return Enum.GetNames(typeof(T)).Length;
+            }
+        }
+    }
+}
